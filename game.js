7bd74fad695788fa7c32cd2cefaaba7f0457b03c
garonbailey <bailegm@gmail.com>
2015-09-24 14:16:30 -0400

    adjusting header positioning and color, built card objects and full deck array

--- game.js
+++ game.js
@@ -1,1 +1,66 @@
-// JS goes here 
+var cards = [
+	two = {
+		name: "two",
+		value: 2
+	},
+	three = {
+		name: "three",
+		value: 3
+	},
+	four = {
+		name: "four",
+		value: 4
+	},
+	five = {
+		name: "five",
+		value: 5
+	},
+	six = {
+		name: "six",
+		value: 6
+	},
+	seven = {
+		name: "seven",
+		value: 7
+	},
+	eight = {
+		name: "eight",
+		value: 8
+	},
+	nine = {
+		name: "nine",
+		value: 9
+	},
+	ten = {
+		name: "ten",
+		value: 10
+	},
+	jack = {
+		name: "jack",
+		value: 10
+	},
+	queen = {
+		name: "queen",
+		value: 10
+	},
+	king = {
+		name: "king",
+		value: 10
+	},
+	ace = {
+		name: "ace",
+		value: 1
+	}
+]
+
+var fullDeck = [];
+
+function setFullDeck () {
+	for (var j = 0; j < 4; j++) {
+		for (var i = 0; i < 13; i++) {	
+			fullDeck.push(cards[i]);
+		}
+	}
+}
+
+setFullDeck();
